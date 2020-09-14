0e46e91713d14d477fef6838a54a6b6a85dc7ecb
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-09-14 22:57:19 +0100

    Setting up NoteRepository

--- app/src/main/java/com/taofeek/practice/NoteDatabase.java
+++ app/src/main/java/com/taofeek/practice/NoteDatabase.java
@@ -1,10 +1,14 @@
 package com.taofeek.practice;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 @Database(entities = Note.class, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
     private static NoteDatabase instance;
@@ -13,8 +17,29 @@ public abstract class NoteDatabase extends RoomDatabase {
         if (instance == null){
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class , "note_database").fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+    private static class PopulateDbAsyncTask extends AsyncTask <Void,Void,Void>{
+        private NoteDao noteDao;
+        private PopulateDbAsyncTask(NoteDatabase db){
+            noteDao = db.noteDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Android Developer","A professional software developer", 4));
+            noteDao.insert(new Note("Taofeek","Android developer", 1));
+            noteDao.insert(new Note("Engr Tee","A professional app developer", 3));
+            return null;
+        }
     }
 }
