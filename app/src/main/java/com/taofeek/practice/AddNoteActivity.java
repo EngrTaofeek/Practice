950c1c0e9d17323923c137a4900cd5de19c69026
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-09-15 23:25:00 +0100

    SAVING NOTE TO THE ROOM DB

--- app/src/main/java/com/taofeek/practice/AddNoteActivity.java
+++ app/src/main/java/com/taofeek/practice/AddNoteActivity.java
@@ -1,12 +1,27 @@
 package com.taofeek.practice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AddNoteActivity extends AppCompatActivity {
+    public static final String EXTRA_TITLE =
+            "com.taofeek.practice.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION =
+            "com.taofeek.practice.EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY =
+            "com.taofeek.practice.EXTRA_PRIORITY";
     private EditText editTextTitle;
     private EditText editTextDescription;
     private NumberPicker numberPickerPriority;
@@ -18,5 +33,45 @@ public class AddNoteActivity extends AppCompatActivity {
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextDescription = findViewById(R.id.edit_text_description);
         numberPickerPriority = findViewById(R.id.number_picker_priority);
+        numberPickerPriority.setMinValue(1);
+        numberPickerPriority.setMaxValue(10);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add New Note");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void saveNote() {
+        String title = editTextTitle.getText().toString().trim();
+        String description = editTextDescription.getText().toString().trim();
+        int priority = numberPickerPriority.getValue();
+        if (title.isEmpty() || description.isEmpty()){
+            Snackbar.make(findViewById(R.id.add_layout),"Please fill both title and description" , Snackbar.LENGTH_LONG).show();
+            return;
+        }
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE,title);
+        data.putExtra(EXTRA_DESCRIPTION,description);
+        data.putExtra(EXTRA_PRIORITY,priority);
+        setResult(RESULT_OK,data);
+        finish();
     }
 }
